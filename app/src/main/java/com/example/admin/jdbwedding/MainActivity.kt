39fdb0fdd78a0cf2f139e06e968b33bcfba731e9
anotherly <xhtmzksk91>
2018-12-24 21:45:42 +0900

    안드로이드 스튜디오와 git연동 성공 커밋2

--- app/src/main/java/com/example/admin/jdbwedding/MainActivity.kt
+++ app/src/main/java/com/example/admin/jdbwedding/MainActivity.kt
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-//test2
+//test3
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
